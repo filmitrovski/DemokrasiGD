f203eaceaa928a4d2f95ebcad514c19597131eae
Filip <filmitrovski@gmail.com>
2022-11-23 11:29:52 +0100

    More contentstuff

--- UI/MainView.gd
+++ UI/MainView.gd
@@ -1,11 +1,11 @@
 extends Camera2D
 
-var step = 64
+var step = 8
 var vwidth = 640
 var vheight = 360
-var limit = 30
+var limit = 32
 	
-func _input(event):
+func _process(_delta):
 	if(get_local_mouse_position().x > vwidth - limit):
 		position.x += step
 	elif(get_local_mouse_position().y > vheight - limit):
